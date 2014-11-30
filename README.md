969c3c6161a9f6599ed1a6976bdef63914146da0
RebekahJ <rebekahjs17@gmail.com>
2014-11-30 22:18:40 +0000

    [UPDATE] Add EWG data

--- README.md
+++ README.md
@@ -16,6 +16,9 @@ foodprint-calc
 + [Food groups](http://fivepercent.us/2008/05/29/link-relative-climate-impact-of-red-meat-vs-other-food-types/)
 + Low carbon diet [wiki](http://en.wikipedia.org/wiki/Low_carbon_diet)
 
+## Sources of data
++ [EWG Meat Eater’s Guide] (http://static.ewg.org/reports/2011/meateaters/pdf/methodology_ewg_meat_eaters_guide_to_health_and_climate_2011.pdf) Use UK value if given, average if not.
+
 ## Issues
 - do for my portion sizes for now?
 - seasonal/geography issues? i.e. include food miles or just focus on production emissions? Would make big difference to e.g. [rice](http://en.wikipedia.org/wiki/Rice#Production), [sugar](http://en.wikipedia.org/wiki/Sugarcane#Production)…, and add dependence on user location. Do for UK user for now…
--- recipe_calc.js
+++ recipe_calc.js
@@ -1,14 +1,31 @@
 var data = [];
-data.push( { food: 'chicken', cpkg: 75 } );
-data.push( { food: 'cheese', cpkg: 100 } );
-data.push( { food: 'arborio', cpkg: 35 } );
+data.push( { food: 'beef', epkg: 39.2 } );
+data.push( { food: 'lamb', epkg: 27.0 } );
+data.push( { food: 'cheese', epkg: 13.5 } );
+data.push( { food: 'pork', epkg: 12.1 } );
+data.push( { food: 'salmon_farm', epkg: 11.9 } );
+data.push( { food: 'turkey', epkg: 10.9 } );
+data.push( { food: 'chicken', epkg: 6.9 } );
+data.push( { food: 'tuna_can', epkg: 6.1 } );
+data.push( { food: 'egg', epkg: 4.8 } );
+data.push( { food: 'potatoes', epkg: 2.9 } );
+data.push( { food: 'rice', epkg: 2.7 } );
+data.push( { food: 'peanut_butter', epkg: 2.5 } );
+data.push( { food: 'nuts', epkg: 2.3 } );
+data.push( { food: 'yogurt', epkg: 2.2 } );
+data.push( { food: 'broccoli', epkg: 2.0 } );
+data.push( { food: 'tofu', epkg: 2.0 } );
+data.push( { food: 'dry_beans', epkg: 2.0 } );
+data.push( { food: 'milk', epkg: 1.9 } );
+data.push( { food: 'tomatoes', epkg: 1.1 } );
+data.push( { food: 'lentils', epkg: 0.9 } );
 
 // recipe stores ingredients and quantities in g
 var rec1 = [];
-rec1.push( { ing: "chicken", qu: 100 } );
-rec1.push( { ing: "arborio", qu: 75 } );
+rec1.push( { ing: "chicken", qug: 100 } );
+rec1.push( { ing: "rice", qug: 75 } );
 
-var carb1 = 0;
+var ems1 = 0;
 
 // loop over recipe entries
 for ( var i = 0; i < rec1.length; i++ ) {
@@ -18,13 +35,13 @@ for ( var i = 0; i < rec1.length; i++ ) {
 
 		// if string matches, ing present in recipe, get carb contribution
 		if ( rec1[i].ing === data[j].food ) {
-			carb1 += rec1[i].qu * data[j].cpkg;
+			ems1 += ( 0.001 * rec1[i].qug ) * data[j].epkg ;
 			break;
 		}
 	} // end ing loop
 } // end recipe loop
 
-console.log( "total carbs is: " + carb1 );
+console.log( "total emissions in kg is: " + ems1 );
 
 
 
