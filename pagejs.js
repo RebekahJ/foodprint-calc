e873281f133ac051337c4eefdf79a59de23d85bd
RebekahJ <rebekahjs17@gmail.com>
2014-12-16 23:02:04 +0000

    [UPDATE] Add button ids, print recipe

--- pagejs.js
+++ pagejs.js
@@ -1,11 +1,18 @@
 var main = function() {
 
+	var recipe1 = [];
+
 	$('.btn').click(function() {
-		alert( "Handler for .click() called." );
-		// show ingredient is selected
-		$(this).toggleClass('active');
+		var quant = prompt("Please enter a quantity of " + this.id + " in g:", 0);
+
+		// show ingredient has been included:
+		
+
   		// display ingredient on right
-  		$('<li>').text('New ingredient').appendTo('.ings');
+  		$('<li>').text(this.id + " " + quant + " g").appendTo('.ings');
+
+  		// add ingredient and quantity to recipe array
+  		recipe1.push( { ing: this.id, qug: quant} );
 	});
 
 };
